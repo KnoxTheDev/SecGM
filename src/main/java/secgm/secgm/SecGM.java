1ec1c6f7590ceba5f1625b1764d8e22ed8c87482
Knox <tuxltuxl001@gmail.com>
2024-08-29 14:12:48 +0530

    Commit

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -1,24 +1,74 @@
 package secgm.secgm;
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SecGM implements ModInitializer {
-	public static final String MOD_ID = "secgm";
+    public static final String MOD_ID = "secgm";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+        registerCommands();
+    }
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    private void registerCommands() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            registerSecGMCommand(dispatcher);
+        });
+    }
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    private void registerSecGMCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("SecGM")
+                .then(CommandManager.argument("mode", IntegerArgumentType.integer(0, 3))
+                        .executes(this::setGameMode)));
+    }
+
+    private int setGameMode(CommandContext<ServerCommandSource> context) {
+        int mode = IntegerArgumentType.getInteger(context, "mode");
+        ServerCommandSource source = context.getSource();
+
+        // Check if the command executor is a player
+        if (source.getEntity() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) source.getEntity();
+            GameMode gameMode;
+
+            switch (mode) {
+                case 0:
+                    gameMode = GameMode.SURVIVAL;
+                    break;
+                case 1:
+                    gameMode = GameMode.CREATIVE;
+                    break;
+                case 2:
+                    gameMode = GameMode.ADVENTURE;
+                    break;
+                case 3:
+                    gameMode = GameMode.SPECTATOR;
+                    break;
+                default:
+                    source.sendFeedback(Text.of("Invalid game mode! Use 0 for Survival, 1 for Creative, 2 for Adventure, or 3 for Spectator."), false);
+                    return 1;
+            }
+
+            // Set the game mode without broadcasting to the server
+            player.setGameMode(gameMode);
+            player.sendMessage(Text.of("Game mode changed to " + gameMode.getName()), false);
+        } else {
+            source.sendFeedback(Text.of("This command can only be executed by a player."), false);
+        }
+
+        return 1;
+    }
 }
