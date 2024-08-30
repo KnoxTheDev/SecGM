fb9f9377302716793caa243d61fea52a5d6ddb99
Knox <tuxltuxl001@gmail.com>
2024-08-30 19:21:00 +0530

    Update SecGM.java

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -10,6 +10,8 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.text.style.Style;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
@@ -73,14 +75,14 @@ public class SecGM implements ModInitializer {
                     gameMode = GameMode.SPECTATOR;
                     break;
                 default:
-                    source.sendFeedback(() -> Text.of("Invalid game mode! Use 0 for Survival, 1 for Creative, 2 for Adventure, or 3 for Spectator."), false);
+                    source.sendFeedback(() -> Text.literal("Invalid game mode! Use 0 for Survival, 1 for Creative, 2 for Adventure, or 3 for Spectator."), false);
                     return 1;
             }
 
             player.changeGameMode(gameMode);
-            player.sendMessage(Text.of("Game mode changed to " + gameMode.getName()), false);
+            player.sendMessage(Text.literal("Game mode changed to " + gameMode.getName()), false);
         } else {
-            source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
+            source.sendFeedback(() -> Text.literal("This command can only be executed by a player."), false);
         }
 
         return 1;
@@ -97,24 +99,18 @@ public class SecGM implements ModInitializer {
             if (player.isInvisible()) {
                 // Unvanish
                 player.setInvisible(false);
-                player.getInventory().armor.forEach(itemStack -> itemStack.setCustomName(Text.of(itemStack.getName().getString()))); // Show worn armor
-                player.getInventory().main.forEach(itemStack -> itemStack.setCustomName(Text.of(itemStack.getName().getString())));  // Show held items
-
                 // Notify all players
-                source.getServer().getPlayerManager().broadcast(Text.of(player.getName().getString() + " joined the game"), false);
-                player.sendMessage(Text.of("You are no longer vanished.").formatted(Formatting.GREEN), false);
+                source.getServer().getPlayerManager().broadcast(Text.literal(player.getName().getString() + " joined the game"), false);
+                player.sendMessage(Text.literal("You are no longer vanished.").styled(style -> style.withColor(Formatting.GREEN)), false);
             } else {
                 // Vanish
                 player.setInvisible(true);
-                player.getInventory().armor.forEach(itemStack -> itemStack.setCustomName(Text.of(""))); // Hide worn armor
-                player.getInventory().main.forEach(itemStack -> itemStack.setCustomName(Text.of("")));  // Hide held items
-
                 // Notify all players
-                source.getServer().getPlayerManager().broadcast(Text.of(player.getName().getString() + " left the game"), false);
-                player.sendMessage(Text.of("You are now vanished.").formatted(Formatting.RED), false);
+                source.getServer().getPlayerManager().broadcast(Text.literal(player.getName().getString() + " left the game"), false);
+                player.sendMessage(Text.literal("You are now vanished.").styled(style -> style.withColor(Formatting.RED)), false);
             }
         } else {
-            source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
+            source.sendFeedback(() -> Text.literal("This command can only be executed by a player."), false);
         }
 
         return 1;
@@ -130,13 +126,13 @@ public class SecGM implements ModInitializer {
 
             // Check if the name contains Unicode characters or spaces
             if (name.matches("^[a-zA-Z0-9_]+$")) {
-                player.setCustomName(Text.of(name));
-                player.sendMessage(Text.of("Nickname changed to " + name), false);
+                player.setCustomName(Text.literal(name));
+                player.sendMessage(Text.literal("Nickname changed to " + name), false);
             } else {
-                source.sendFeedback(() -> Text.of("Invalid nickname! Only letters, numbers, and underscores are allowed."), false);
+                source.sendFeedback(() -> Text.literal("Invalid nickname! Only letters, numbers, and underscores are allowed."), false);
             }
         } else {
-            source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
+            source.sendFeedback(() -> Text.literal("This command can only be executed by a player."), false);
         }
 
         return 1;
