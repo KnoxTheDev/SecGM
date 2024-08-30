8e773a2443325237a5c34ab12a577574eefd5f20
Knox <tuxltuxl001@gmail.com>
2024-08-30 22:37:15 +0530

    Delete src/main/java/secgm/secgm/TextHelper.java

--- src/main/java/secgm/secgm/TextHelper.java
+++
@@ -1,8 +0,0 @@
-package secgm.secgm;
-
-import net.minecraft.item.ItemStack;
-
-public interface TextHelper {
-    void setCustomName(ItemStack itemStack, String name);
-    String getName(ItemStack itemStack);
-}
