d22187d8d7aacbb2a730dc1e24501673075c210f
Knox <tuxltuxl001@gmail.com>
2024-08-30 20:07:18 +0530

    Update TextHelperV1_16.java

--- src/main/java/secgm/secgm/TextHelperV1_16.java
+++ src/main/java/secgm/secgm/TextHelperV1_16.java
@@ -1,16 +1,18 @@
 package secgm.secgm;
 
-import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText; // Use LiteralText for Minecraft 1.16
 import net.minecraft.item.ItemStack;
 
 public class TextHelperV1_16 implements TextHelper {
     @Override
     public void setCustomName(ItemStack itemStack, String name) {
-        itemStack.setCustomName(Text.of(name));
+        // Set custom name using LiteralText for 1.16
+        itemStack.setCustomName(new LiteralText(name));
     }
 
     @Override
     public String getName(ItemStack itemStack) {
+        // Get the name as a string
         return itemStack.getName().getString();
     }
 }
