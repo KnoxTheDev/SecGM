e88854479a66814bc9cc6722d9656a1ae74be984
Knox <tuxltuxl001@gmail.com>
2024-08-31 14:27:32 +0530

    Update SecGM.java

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -1,135 +1,145 @@
-package com.secgm.secgm;
+package secgm.secgm;
 
-import net.minecraft.server.command.Command;
-import net.minecraft.server.command.CommandRegistry;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
-import net.minecraft.util.Formatting;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.Arrays;
+import net.minecraft.world.GameMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SecGM implements ModInitializer {
-
     public static final String MOD_ID = "secgm";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-
-    private static boolean isVanished = false;
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("SecGM mod initialized!");
-
-        // Register the /secgm command
-        try {
-            CommandRegistry.registerCommand("secgm", new SecGMCommand());
-            CommandRegistry.registerCommand("vanish", new VanishCommand());
-        } catch (Exception e) {
-            LOGGER.error("Error registering commands:", e);
-        }
+        LOGGER.info("Hello Fabric world!");
+        registerCommands();
     }
 
-    public static class SecGMCommand implements Command {
-        @Override
-        public int execute(ServerCommandSource source, String[] args) {
-            if (!(source.getEntity() instanceof ServerPlayerEntity)) {
-                source.sendError(new LiteralText("Only players can use this command!"));
-                return 1;
-            }
-
-            try {
-                ServerPlayerEntity player = (ServerPlayerEntity) source.getEntity();
-
-                if (args.length == 0) {
-                    source.sendError(new LiteralText("Usage: /secgm <0|1|2|3>"));
-                    return 1;
-                }
-
-                int gamemode;
-                try {
-                    gamemode = Integer.parseInt(args[0]);
-                } catch (NumberFormatException e) {
-                    source.sendError(new LiteralText("Invalid gamemode!"));
-                    return 1;
-                }
-
-                if (gamemode < 0 || gamemode > 3) {
-                    source.sendError(new LiteralText("Invalid gamemode!"));
-                    return 1;
-                }
-
-                // Change gamemode discreetly
-                player.setGameMode(gamemode);
-                source.sendFeedback(new LiteralText("Gamemode changed to " + gamemode).formatted(Formatting.GREEN), false);
-                return 0;
-            } catch (Exception e) {
-                LOGGER.error("Error executing /secgm command:", e);
-                source.sendError(new LiteralText("An error occurred while executing the command!"));
-                return 1;
-            }
-        }
+    private void registerCommands() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            registerSecgmCommand(dispatcher);
+            registerVanishCommand(dispatcher);
+        });
     }
 
-    public static class VanishCommand implements Command {
-        @Override
-        public int execute(ServerCommandSource source, String[] args) {
-            if (!(source.getEntity() instanceof ServerPlayerEntity)) {
-                source.sendError(new LiteralText("Only players can use this command!"));
-                return 1;
+    private void registerSecgmCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("secgm")
+                .then(CommandManager.argument("gameModeId", IntegerArgumentType.integer(0, 3))
+                        .executes(this::executeSecgmCommand)));
+    }
+
+    private void registerVanishCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("vanish")
+                .executes(this::executeVanishCommand));
+    }
+
+    private int executeSecgmCommand(CommandContext<ServerCommandSource> context) {
+        int gameModeId = IntegerArgumentType.getInteger(context, "gameModeId");
+        ServerCommandSource source = context.getSource();
+
+        // Check if the command executor is a player
+        if (source.getEntity() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) source.getEntity();
+            GameMode gameMode;
+
+            switch (gameModeId) {
+                case 0:
+                    gameMode = GameMode.SURVIVAL;
+                    break;
+                case 1:
+                    gameMode = GameMode.CREATIVE;
+                    break;
+                case 2:
+                    gameMode = GameMode.ADVENTURE;
+                    break;
+                case 3:
+                    gameMode = GameMode.SPECTATOR;
+                    break;
+                default:
+                    source.sendFeedback(() -> Text.of("Invalid game mode ID."), false);
+                    return 0;
             }
 
-            try {
-                ServerPlayerEntity player = (ServerPlayerEntity) source.getEntity();
-
-                if (isVanished) {
-                    unVanish(player);
-                    isVanished = false;
-                } else {
-                    vanish(player);
-                    isVanished = true;
-                }
-
-                return 0;
-            } catch (Exception e) {
-                LOGGER.error("Error executing /vanish command:", e);
-                source.sendError(new LiteralText("An error occurred while executing the command!"));
-                return 1;
-            }
+            player.changeGameMode(gameMode);
+            player.sendMessage(Text.of("Game mode changed to " + gameMode.getName()), false);
+        } else {
+            source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
         }
 
-        private void vanish(ServerPlayerEntity player) {
-            try {
-                // Fake leave message with yellow formatting
-                Text leaveMessage = new LiteralText(player.getEntityName() + " left the game.").setStyle(Style.EMPTY.withColor(TextColor.YELLOW));
-                player.getServer().getPlayerManager().broadcastChatMessage(leaveMessage, false);
+        return 1;
+    }
 
-                // Hide the player from the server
-                player.setInvisible(true);
-                player.setInvulnerable(true);
-                player.setSilent(true);
-            } catch (Exception e) {
-                LOGGER.error("Error vanishing player:", e);
-            }
-        }
+    private int executeVanishCommand(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
 
-        private void unVanish(ServerPlayerEntity player) {
-            try {
-                // Fake join message with yellow formatting
-                Text joinMessage = new LiteralText(player.getEntityName() + " joined the game.").setStyle(Style.EMPTY.withColor(TextColor.YELLOW));
-                player.getServer().getPlayerManager().broadcastChatMessage(joinMessage, false);
+        // Check if the command executor is a player
+        if (source.getEntity() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) source.getEntity();
 
-                // Unhide the player from the server
-                player.setInvisible(false);
+            // Toggle vanish status
+            if (player.hasStatusEffect(StatusEffects.INVISIBILITY)) {
+                player.removeStatusEffect(StatusEffects.INVISIBILITY);
                 player.setInvulnerable(false);
-                player.setSilent(false);
-            } catch (Exception e) {
-                LOGGER.error("Error unvanishing player:", e);
+                player.sendMessage(Text.of("You are now visible and vulnerable."), false);
+
+                // Make player's items and armor visible
+                player.getInventory().forEach(stack -> {
+                    if (stack != null) {
+                        stack.setGlowingTag(stack.getOrCreateTag());
+                    }
+                });
+                player.getArmorItems().forEach(stack -> {
+                    if (stack != null) {
+                        stack.setGlowingTag(stack.getOrCreateTag());
+                    }
+                });
+
+                // Allow mobs to target the player again
+                player.getWorld().getEntities().forEach(entity -> {
+                    if (entity instanceof MobEntity) {
+                        ((MobEntity) entity).setTarget(player);
+                    }
+                });
+            } else {
+                player.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, Integer.MAX_VALUE, 0, false, false));
+                player.setInvulnerable(true);
+                player.sendMessage(Text.of("You are now invisible and invulnerable."), false);
+
+                // Make player's items and armor invisible
+                player.getInventory().forEach(stack -> {
+                    if (stack != null) {
+                        stack.setGlowingTag(null);
+                    }
+                });
+                player.getArmorItems().forEach(stack -> {
+                    if (stack != null) {
+                        stack.setGlowingTag(null);
+                    }
+                });
+
+                // Prevent mobs from targeting the player
+                player.getWorld().getEntities().forEach(entity -> {
+                    if (entity instanceof MobEntity) {
+                        ((MobEntity) entity).setTarget(null);
+                    }
+                });
             }
+        } else {
+            source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
         }
+
+        return 1;
     }
-}
+        }
