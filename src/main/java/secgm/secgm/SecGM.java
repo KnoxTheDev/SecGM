f42ec5af9854a401b59229979a2be15981dcc9c7
Knox <tuxltuxl001@gmail.com>
2024-08-31 10:06:37 +0530

    Update SecGM.java

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -5,17 +5,11 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.network.packet.s2c.play.EntityEquipmentUpdateS2CPacket;
-import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.EquipmentSlot.Type;
+import net.minecraft.text.TextColor;
 import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +27,7 @@ public class SecGM implements ModInitializer {
     private void registerCommands() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             registersecgmCommand(dispatcher);
-            registervanishCommand(dispatcher);
+            registerVanishCommand(dispatcher);
         });
     }
 
@@ -41,11 +35,6 @@ public class SecGM implements ModInitializer {
         dispatcher.register(CommandManager.literal("secgm")
                 .then(CommandManager.argument("mode", IntegerArgumentType.integer(0, 3))
                         .executes(this::setGameMode)));
-    }
-
-    private void registervanishCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(CommandManager.literal("vanish")
-                .executes(this::vanishPlayer));
     }
 
     private int setGameMode(CommandContext<ServerCommandSource> context) {
@@ -59,23 +48,25 @@ public class SecGM implements ModInitializer {
 
             switch (mode) {
                 case 0:
-                    player.changeGameMode(GameMode.SURVIVAL);
+                    gameMode = GameMode.SURVIVAL;
                     break;
                 case 1:
-                    player.changeGameMode(GameMode.CREATIVE);
+                    gameMode = GameMode.CREATIVE;
                     break;
                 case 2:
-                    player.changeGameMode(GameMode.ADVENTURE);
+                    gameMode = GameMode.ADVENTURE;
                     break;
                 case 3:
-                    player.changeGameMode(GameMode.SPECTATOR);
+                    gameMode = GameMode.SPECTATOR;
                     break;
                 default:
                     source.sendFeedback(() -> Text.of("Invalid game mode! Use 0 for Survival, 1 for Creative, 2 for Adventure, or 3 for Spectator."), false);
                     return 1;
             }
 
-            player.sendMessage(Text.of("Game mode changed to " + player.interactionManager.getGameMode().getName()), false);
+            // Use changeGameMode() instead of setGameMode() if setGameMode() is unavailable
+            player.changeGameMode(gameMode);
+            player.sendMessage(Text.of("Game mode changed to " + gameMode.getName()), false);
         } else {
             source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
         }
@@ -83,43 +74,48 @@ public class SecGM implements ModInitializer {
         return 1;
     }
 
-    private int vanishPlayer(CommandContext<ServerCommandSource> context) {
+    private void registerVanishCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("vanish")
+                .executes(this::vanishCommand));
+    }
+
+    private int vanishCommand(CommandContext<ServerCommandSource> context) {
         ServerCommandSource source = context.getSource();
 
         // Check if the command executor is a player
         if (source.getEntity() instanceof ServerPlayerEntity) {
             ServerPlayerEntity player = (ServerPlayerEntity) source.getEntity();
 
-            // Toggle vanish mode
-            player.setInvisible(!player.isInvisible());
-
-            // Hide player from tab list
-            PlayerListS2CPacket packet = new PlayerListS2CPacket(PlayerListS2CPacket.Action.REMOVE_PLAYER, player);
-            player.server.getPlayerManager().sendToAll(packet);
-
-            // Fake realistic joining and leaving messages
+            // Check if the player is already vanished
             if (player.isInvisible()) {
-                player.server.getPlayerManager().broadcastChatMessage(Text.of(player.getName() + " left the game"), net.minecraft.network.MessageType.SYSTEM, player.getUUID());
+                player.setInvisible(false);
+                player.sendMessage(Text.of("You are now visible."), false);
+
+                // Re-add the player to the player list
+                player.getServer().getPlayerManager().sendPlayerListUpdate(player);
+
+                // Re-add the player to the entity tracker
+                player.getServer().getEntityTracker().updateTracking(player);
+
+                // Send a fake join message
+                player.getServer().getPlayerManager().broadcastChatMessage(Text.of(player.getName().getString() + " joined the game").styled(style -> style.withColor(TextColor.YELLOW)), false);
             } else {
-                player.server.getPlayerManager().broadcastChatMessage(Text.of(player.getName() + " joined the game"), net.minecraft.network.MessageType.SYSTEM, player.getUUID());
+                player.setInvisible(true);
+                player.sendMessage(Text.of("You are now invisible."), false);
+
+                // Remove the player from the player list
+                player.getServer().getPlayerManager().sendPlayerListUpdate(player, false);
+
+                // Remove the player from the entity tracker
+                player.getServer().getEntityTracker().remove(player);
+
+                // Send a fake leave message
+                player.getServer().getPlayerManager().broadcastChatMessage(Text.of(player.getName().getString() + " left the game").styled(style -> style.withColor(TextColor.YELLOW)), false);
             }
-
-            // Hide items held in both hands
-            player.server.getPlayerManager().sendToAll(new EntityEquipmentUpdateS2CPacket(player.getId(), EquipmentSlot.MAINHAND, ItemStack.EMPTY));
-            player.server.getPlayerManager().sendToAll(new EntityEquipmentUpdateS2CPacket(player.getId(), EquipmentSlot.OFFHAND, ItemStack.EMPTY));
-
-            // Hide worn items
-            for (EquipmentSlot slot : EquipmentSlot.values()) {
-                if (slot.getType() == EquipmentSlot.Type.ARMOR) {
-                    player.server.getPlayerManager().sendToAll(new EntityEquipmentUpdateS2CPacket(player.getId(), slot, ItemStack.EMPTY));
-                }
-            }
-
-            player.sendMessage(Text.of("Vanish mode " + (player.isInvisible() ? "enabled" : "disabled")), false);
         } else {
             source.sendFeedback(() -> Text.of("This command can only be executed by a player."), false);
         }
 
         return 1;
     }
-}
+                                       }
