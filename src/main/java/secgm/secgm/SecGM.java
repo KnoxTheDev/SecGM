4b0767e3fc61213f4a1a6042d269c7c5cbac971e
Knox <tuxltuxl001@gmail.com>
2024-08-31 17:55:55 +0530

    Update SecGM.java

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.Packet;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
