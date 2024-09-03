7c47e2541bb857edba17cc23645ed85e7b187903
Knox <tuxltuxl001@gmail.com>
2024-09-03 21:52:21 +0530

    Update SecGM.java

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -1,29 +1,28 @@
 package secgm.secgm;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.input.Input;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.util.MovementInput;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 public class SecGM implements ClientModInitializer {
     private MinecraftClient mc = MinecraftClient.getInstance();
-    private KeyBinding flyHackKey;
+    private Input input;
 
     private boolean freecamEnabled = false;
     private double storedX, storedY, storedZ;
 
     @Override
     public void onInitializeClient() {
-        flyHackKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("FlyHack", InputUtil.Type.KEYSYM, 70, "secgm.secgm")); // F key
+        input = mc.input;
+        KeyBindingHelper.registerKeyBinding(new KeyBinding("FlyHack", InputUtil.Type.KEYSYM, 70, "secgm.secgm")); // F key
         ClientTickEvents.END_CLIENT_TICK.register(this::onTick);
     }
 
     private void onTick() {
-        if (flyHackKey.wasPressed()) {
+        if (input.isKeyPressed(70)) { // F key
             if (!freecamEnabled) {
                 storedX = mc.player.getX();
                 storedY = mc.player.getY();
@@ -40,11 +39,11 @@ public class SecGM implements ClientModInitializer {
         }
 
         if (freecamEnabled) {
-            mc.player.input = new MovementInput();
-            mc.player.input.jump = false;
-            mc.player.input.sneak = false;
-            mc.player.input.moveForward = 0;
-            mc.player.input.moveStrafe = 0;
+            input = new Input(mc);
+            input.jump = false;
+            input.sneak = false;
+            input.moveForward = 0;
+            input.moveStrafe = 0;
         }
     }
 }
