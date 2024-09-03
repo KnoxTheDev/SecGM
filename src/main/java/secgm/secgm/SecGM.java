b394b4955cff5bf25bb0334d4454d6d8344a2aaa
Knox <tuxltuxl001@gmail.com>
2024-09-03 22:00:18 +0530

    Update SecGM.java

--- src/main/java/secgm/secgm/SecGM.java
+++ src/main/java/secgm/secgm/SecGM.java
@@ -2,7 +2,6 @@ package secgm.secgm;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.Input;
-import net.minecraft.client.input.MovementInput;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -11,21 +10,21 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 public class SecGM implements ClientModInitializer {
     private MinecraftClient mc = MinecraftClient.getInstance();
-    private MovementInput movementInput;
+    private Input input;
 
     private boolean freecamEnabled = false;
     private double storedX, storedY, storedZ;
 
     @Override
     public void onInitializeClient() {
-        movementInput = mc.getInput().getMovementInput();
+        input = mc.getInput();
         KeyBinding flyHackKeyBinding = new KeyBinding("FlyHack", KeyBindingHelper.getOrCreate("secgm.secgm"), 70); // F key
         KeyBindingHelper.registerKeyBinding(flyHackKeyBinding);
         ClientTickEvents.END_CLIENT_TICK.register(this::onTick);
     }
 
     private void onTick() {
-        if (mc.getInput().isKeyPressed(flyHackKeyBinding.getDefaultKey())) { // F key
+        if (input.isKeyPressed(flyHackKeyBinding.getDefaultKey())) { // F key
             if (!freecamEnabled) {
                 storedX = mc.player.getX();
                 storedY = mc.player.getY();
@@ -42,10 +41,10 @@ public class SecGM implements ClientModInitializer {
         }
 
         if (freecamEnabled) {
-            movementInput.jump = false;
-            movementInput.sneak = false;
-            movementInput.moveForward = 0;
-            movementInput.moveStrafe = 0;
+            input.jump = false;
+            input.sneak = false;
+            input.moveForward = 0;
+            input.moveStrafe = 0;
         }
     }
 }
